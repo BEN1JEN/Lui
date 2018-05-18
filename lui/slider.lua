a340a20cacf80fc5281a1d49cd434e5d3c5e757f
BEN1JEN <benpheard@gmail.com>
2018-05-17 23:14:16 -0600

    fully implemented sliders

--- lui/slider.lua
+++ lui/slider.lua
@@ -1,5 +1,6 @@
 local slider = {}
 local sliders = {}
+local mouseDLast = false
 
 function slider.newSlider(x, y, length, colour, upright, min, max)
 
@@ -13,21 +14,51 @@ function slider.newSlider(x, y, length, colour, upright, min, max)
 		max = 1
 	end
 
-	sliders[#sliders+1] = {x=x, y=y, colour=colour, upright=upright, pos=0.3, min=min, max=max, grabbed=false, length=length}
+	sliders[#sliders+1] = {x=x, y=y, colour=colour, upright=upright, pos=min, min=min, max=max, grabbed=false, length=length}
 
 	return #sliders
 end
 
 function slider.updatePositions(delta)
+
+	local mouseX, mouseY = love.mouse.getPosition()
+	local mouseD = love.mouse.isDown(1)
+	for id, sld in ipairs(sliders) do
+		local position = (sld.pos/(sld.max-sld.min)*(sld.length-14))+2
+		if not sld.grabbed then
+			if mouseD and not mouseDLast and mouseX > sld.x+position and mouseX < sld.x+position+10 and mouseY > sld.y+2 and mouseY < sld.y+27 then
+				sld.grabbed = true
+			end
+		else
+			if not mouseD then
+				sld.grabbed = false
+			end
+
+			sld.pos = (mouseX - sld.x) / sld.length * (sld.max-sld.min)
+
+			if sld.pos >= sld.max then
+				sld.pos = sld.max
+			elseif sld.pos <= sld.min then
+				sld.pos = sld.min
+			end
+		end
+	end
+	mouseDLast = mouseD
+
 end
 
 function slider.drawSliders()
+
 	for id, sld in ipairs(sliders) do
 		love.graphics.setColor(sld["colour"][1]/2, sld["colour"][2]/2, sld["colour"][3]/2)
 		love.graphics.rectangle("fill", sld.x, sld.y, sld.length, 25)
 		love.graphics.setColor(sld.colour)
-		love.graphics.rectangle("fill", sld.x+(sld.pos/(sld.max-sld.min)*sld.length-4)+2, sld.y+2, 10, 21)
+		if sld.grabbed then
+			love.graphics.setColor(sld["colour"][1]*0.1, sld["colour"][2]*0.1, sld["colour"][3]*0.1)
+		end
+		love.graphics.rectangle("fill", sld.x+(sld.pos/(sld.max-sld.min)*(sld.length-14))+2, sld.y+2, 10, 21)
 	end
+
 end
 
 return slider
